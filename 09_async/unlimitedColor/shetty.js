a8b313ba9235915534d37d0690857e8a76a5e2c9
dhanushshetty1 <dhanushshetty106@gmail.com>
2025-08-07 00:41:43 +0530

    Project and code on Async

---
+++ 09_async/unlimitedColor/shetty.js
@@ -0,0 +1,27 @@
+//generate a random color
+const randomColor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = '#'
+    for(let i=0; i<6; i++){
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color
+}
+
+let interVal;
+const startChangingColor = function(){
+    if(!interVal){
+        interVal = setInterval(changebgColor, 500)
+    }
+
+    function changebgColor(){
+         document.body.style.backgroundColor = randomColor();
+    } 
+}
+const stopChangingColor = function(){
+    clearInterval(interVal)
+    interVal = null;
+}
+
+document.querySelector("#start").addEventListener('click', startChangingColor)
+document.querySelector("#stop").addEventListener('click', stopChangingColor)
